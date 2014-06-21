0ca276b413bae4d7354487921261c917d02483f3
Henry Kielmann <henrykielmann@gmail.com>
2014-06-21 12:21:59 +0200

    Initial commit.

---
+++ desktop/src/kammerjaeger/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package kammerjaeger.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import kammerjaeger.KammerJaegerGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new KammerJaegerGame(), config);
+	}
+}
